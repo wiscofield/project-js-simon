a41b4ac692855a82ab38712d8d0bf740f45d3cd2
Jesse Farmer <jesse@20bits.com>
2020-02-24 14:47:44 -0800

    Browser console only Simon game

--- site/buttons.js
+++ site/buttons.js
@@ -1,15 +1,44 @@
+let simon = newSimon();
+
+simonStart(simon);
+
+if (simonChallengeIsFresh(simon)) {
+  console.log('Colors:', simon.challenge);
+}
+
+function pressButton(simon, buttonColor) {
+  if (simonIsMoveCorrect(simon, buttonColor)) {
+    simon.step += 1;
+
+    if (simonIsChallengeComplete(simon)) {
+      let newColor = getRandomElement(SIMON_COLORS);
+
+      simon.step = 0;
+      simon.challenge.push(newColor);
+
+      console.log('Colors:', simon.challenge);
+    }
+  } else {
+    simonEndGame(simon);
+
+    let score = simonGetScore(simon);
+
+    console.log(`Game over! Your score is ${score}.`);
+  }
+}
+
 $('.simon-button.green').on('click', function() {
-  alert('You clicked the green button');
+  pressButton(simon, 'green');
 });
 
 $('.simon-button.blue').on('click', function() {
-  alert('You clicked the blue button');
+  pressButton(simon, 'blue');
 });
 
 $('.simon-button.yellow').on('click', function() {
-  alert('You clicked the yellow button');
+  pressButton(simon, 'yellow');
 });
 
 $('.simon-button.red').on('click', function() {
-  alert('You clicked the red button');
+  pressButton(simon, 'red');
 });
--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,50 @@
-// This file contains the game logic.
-// All the event-listening should happen in buttons.js
+function newSimon() {
+  return {
+    state: 'fresh',
+    challenge: [],
+    step: 0,
+  };
+}
+
+let SIMON_COLORS = [
+  'red',
+  'blue',
+  'yellow',
+  'green',
+];
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function simonChallengeIsFresh(simon) {
+  return simon.step === 0;
+}
+
+function simonIsChallengeComplete(simon) {
+  return simon.step === simon.challenge.length;
+}
+
+function simonIsGameFinished(simon) {
+  return simon.state === 'finished';
+}
+
+function simonEndGame(simon) {
+  simon.state = 'finished';
+}
+
+function simonGetScore(simon) {
+  return simon.challenge.length - 1;
+}
+
+function simonIsMoveCorrect(simon, move) {
+  return simon.challenge[simon.step] === move;
+}
+
+function simonStart(simon) {
+  let newColor = getRandomElement(SIMON_COLORS);
+
+  simon.step = 0;
+  simon.challenge = [newColor];
+  simon.state = 'playing';
+}
