3f6d1d79e8222bf2474296392af31818e3e23b2c
Will Scofield <wiscofield@davidson.edu>
2020-07-16 19:09:34 -0400

    more work on game logic and loop

--- buttons.js
+++ buttons.js
@@ -1,60 +1,58 @@
-var clicksTrack = 0
-
 let greenButton = document.querySelector('.simon-button.green');
 greenButton.addEventListener('click', function() {
   if(allowInput) {
-    greenButton.classList.add('light-up');
-    clicksTrack++;
-    document.getElementById('clicks-track').innerHTML =('Clicks: ' + clicksTrack);
-    setTimeout(turnOffLight, 250);
+    lightUp('.green');
     playSound(300, 250);
-
-    buttonPushed = true;
+    clicksTrack++;
     buttonValue = 1;
+
+    userTurn();
   }
 });
 
 let redButton = document.querySelector('.simon-button.red');
 redButton.addEventListener('click', function() {
   if(allowInput) {
-    redButton.classList.add('light-up');
-    clicksTrack++;
-    document.getElementById('clicks-track').innerHTML =('Clicks: ' + clicksTrack);
-    setTimeout(turnOffLight, 250);
+    lightUp('.red')
     playSound(349, 250);
-
-    buttonPushed = true;
+    clicksTrack++;
     buttonValue = 2;
+
+    userTurn();
   }
 });
 
 let yellowButton = document.querySelector('.simon-button.yellow');
 yellowButton.addEventListener('click', function() {
   if(allowInput) {
-    yellowButton.classList.add('light-up');
-    clicksTrack++;
-    document.getElementById('clicks-track').innerHTML =('Clicks: ' + clicksTrack);
-    setTimeout(turnOffLight, 250);
+    lightUp('.yellow');
     playSound(440, 250);
-
-    buttonPushed = true;
+    clicksTrack++;
     buttonValue = 3;
+
+    userTurn();
   }
 });
 
 let blueButton = document.querySelector('.simon-button.blue');
 blueButton.addEventListener('click', function() {
   if(allowInput) {
-    blueButton.classList.add('light-up');
-    clicksTrack++;
-    document.getElementById('clicks-track').innerHTML =('Clicks: ' + clicksTrack);
-    setTimeout(turnOffLight, 250);
+    lightUp('.blue');
     playSound(523, 250);
-
-    buttonPushed = true;
+    clicksTrack++;
     buttonValue = 4;
+
+    userTurn();
   }
 });
+
+
+//HELPER FUNCTIONS
+function lightUp(color) {
+  let button = document.querySelector(color);
+  button.classList.add('light-up');
+  setTimeout(turnOffLight, 250);
+}
 
 function turnOffLight() {
   greenButton.classList.remove('light-up');
@@ -62,7 +60,6 @@ function turnOffLight() {
   yellowButton.classList.remove('light-up');
   blueButton.classList.remove('light-up');
 }
-
 
 function playSound(freq, time) {
   let context = new AudioContext();
--- simon.js
+++ simon.js
@@ -2,34 +2,88 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
 
-var sequence = new Int16Array();
+var clicksTrack = 0;
+var sequence = []; //computer array
+var userSequence = []; //user array
 var allowInput = false;
-var buttonValue = 0;
-var buttonPushed = false;
+let buttonValue = 0;
 
-gameManager();
+computerTurn();
 
-function gameManager() {
-  var rand = getRandom();
-  console.log(rand);
-  //Add to sequence
-
-  let a = checkInput(rand);
-  console.log(a);
+//TURN FUNCTIONS
+function computerTurn() {
+  allowInput = false;
+  let rand = getRandom();
+  sequence.push(rand);
+  setTimeout(function() {
+    lights(0);
+  }, 250);
+  allowInput = true;
 }
 
+function lights(num) {
+  switch(sequence[num]) {
+    case 1:
+      lightUp('.green');
+      break;
+    case 2:
+      lightUp('.red');
+      break;
+    case 3:
+      lightUp('.yellow');
+      break;
+    case 4:
+      lightUp('.blue');
+  }
+  num++;
+
+  if(num < sequence.length) {
+    setTimeout(function() {
+      lights(num);
+    }, 250);
+  }
+}
+
+function userTurn() {
+  if(!checkButton(buttonValue, sequence[clicksTrack - 1])) {
+    loseGame();
+    clicksTrack = 0;
+  }
+
+  userSequence.push(buttonValue);
+
+  if(userSequence.length == sequence.length) {
+    clicksTrack = 0;
+    setTimeout(computerTurn, 500);
+  }
+}
+
+function loseGame(){
+  allowInput = false;
+  console.log('GAME LOST');
+}
+
+//HELPER FUNCTIONS
 function getRandom() {
   //generate random 1-4
-return Math.floor((Math.random() * 4) + 1);
+  return Math.floor((Math.random() * 4) + 1);
 }
 
-function checkInput(rand) {
-  allowInput = true;
-  while(!buttonPushed) {
-    let input = buttonValue;
-  }
-  allowInput = false;
-  buttonPushed = false;
-  buttonValue = 0;
-  return rand == input;
+function checkButton(a, b) {
+  console.log("CHECKING INPUT");
+  return a == b;
+}
+
+//BONUS
+function lightUp(color) {
+  let button = document.querySelector(color);
+  button.classList.add('light-up');
+  setTimeout(turnOffLight, 250);
+}
+
+function turnOffLight() {
+  greenButton.classList.remove('light-up');
+  redButton.classList.remove('light-up');
+  yellowButton.classList.remove('light-up');
+  blueButton.classList.remove('light-up');
 }
